6f0fac2ee64db8b364d56072371b4c7c8dc6995f
Ayu22 <ayushi.kp@gmail.com>
2015-05-27 18:25:39 +0530

    Add HTML,JS,CSS files for first email created

---
+++ email.js
@@ -0,0 +1,12 @@
+angular.module(email-app,[]){
+	
+}
+
+function($scope) = MyController{
+	$scope.emails = [
+		{name:Ayushi,age:20,id:ayushi.kp@gmail.com},
+		{name:Kirti,age:21,id:kirti.ultimate@gmail.com},
+		{name:Utkarsh,age:22,id:utkarsh.rocks@gmail.com},
+		{name:Ashish,age:20,id:babaji@gmail.com}
+	];
+}
